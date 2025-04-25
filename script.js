2621a4b5f522e17472a033288bad400d29d3d490
Úlpio Netto <98027343+Ulpio@users.noreply.github.com>
2025-04-25 15:50:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let i = 0
+
+function incrementar(){
+    i ++
+    document.getElementById("contador").innerText = i
+}
+
+function zerar(){
+    i = 0
+    document.getElementById("contador").innerText = i
+}
